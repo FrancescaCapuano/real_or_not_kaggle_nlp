053e11068fa2d04c7c96a9acd145af614b43197e
Francesca Capuano <f.capuano1991@gmail.com>
2020-10-06 20:05:58 +0200

    updated word2vec

--- word2vec.py
+++ word2vec.py
@@ -9,6 +9,8 @@ import string
 import pdb 
 from nltk.corpus import stopwords
 from sklearn.preprocessing import OneHotEncoder
+import numpy as np
+import tensorflow as tf
 
 def word2vec():
 	pass
@@ -18,42 +20,54 @@ if __name__ == '__main__':
 	# Download Michael, download!
 	# nltk.download('gutenberg')
 	# Fetch corpus from nltk
-	my_words = nltk.corpus.gutenberg.words("austen-emma.txt")
+	my_sentences = nltk.corpus.gutenberg.sents("austen-emma.txt")
 	
 	# Remove punctuation	
-	my_words = [w.translate(str.maketrans('', '', string.punctuation)) for w in my_words]
-	my_words = [w for w in my_words if w != ""]
+	my_sentences = [[w.translate(str.maketrans('', '', string.punctuation)) for w in sent] for sent in my_sentences[:200]]
+	# remove empty strings
+	my_sentences = [[w for w in sent_words if w != ""] for sent_words in my_sentences]
 
 	# Remove stopwords
 	stop_words = set(stopwords.words('english'))   
-	my_words = [w for w in my_words if not w in stop_words] 
+	my_sentences = [[w for w in sent if not w in stop_words] for sent in my_sentences]
 
 	# Lemmatize
 	lemmatizer = WordNetLemmatizer() 
-	my_words = [lemmatizer.lemmatize(w) for w in my_words]	  
+	my_sentences = [[lemmatizer.lemmatize(w) for w in sent] for sent in my_sentences]
 
 	# Create unique id for each token
-	word_to_id = {token: idx for idx, token in enumerate(set(my_words))}
+	word_to_id = {token: idx for idx, token in enumerate(set([w for sent in my_sentences for w in sent]))}
 
 	# Convert text to list of word ids
-	token_ids = [word_to_id[token] for token in my_words]
+	sentences_ids = [[word_to_id[token] for token in sent] for sent in my_sentences]
 
-	# NEED TO FETCH SENTENCES INSTEAD OF WORDS
-	# Training word pairs
+	# Building word pairs
 	window_size = 2
 	data = []
-	for i, w in enumerate(my_words[2:5]):
-		for j in range(-window_size,window_size+1):
-			if j != 0:
-				data.append([w, my_words[i + 2 + j]])
 
-	print(data[:20])
-	print(my_words[:20])
+	for sent in sentences_ids[:3]:
+		for i, w in enumerate(sent):
+			for j in range(-window_size,window_size+1):
+				if j != 0:
+					if 0 <=  i+ j < len(sent):
+						data.append([w, sent[i + j]])
 
-	data = [[w_centre, w_context] for w_centre in w]
 
-	pdb.set_trace()
 
 	# One hot encodings
-	vec = OneHotEncoder()
-	X = vec.fit_transform(token_ids)
+	n_tokens = len(word_to_id.keys())
+	one_hot_encodings = [np.eye(n_tokens)[pair] for pair in data]
+
+	# model creation
+	model = keras.Sequential()
+	model.add(keras.Input(shape=(n_tokens,)))
+	model.add(layers.Dense(300, activation="relu"))
+	model.add(layers.Dense(n_tokens))
+	
+	model.compile(optimizer='adam',
+              loss=tf.keras.losses.categorical_crossentropy(),
+              metrics=['accuracy'])
+
+	model.fit(train, label)
+
+	
