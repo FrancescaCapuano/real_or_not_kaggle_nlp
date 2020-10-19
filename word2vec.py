8158d7dee057c223bcc3e25be8c373f81be0a5b5
Michael Stettler <michael.stettler@cin.uni-tuebingen.de>
2020-10-19 19:46:53 +0200

    added print info

--- word2vec.py
+++ word2vec.py
@@ -25,13 +25,14 @@ if __name__ == '__main__':
 	# Fetch corpus from nltk
 	my_sentences = nltk.corpus.gutenberg.sents("austen-emma.txt")
 
-	# n_sentences = 50
-	
-	# Remove punctuation	
-	# my_sentences = [[w.translate(str.maketrans('', '', string.punctuation)) for w in sent] for sent in my_sentences[:n_sentences]]
-	my_sentences = [[w.translate(str.maketrans('', '', string.punctuation)) for w in sent] for sent in my_sentences]
+	n_sentences = 5
+
+	# Remove punctuation
+	my_sentences = [[w.translate(str.maketrans('', '', string.punctuation)) for w in sent] for sent in my_sentences[:n_sentences]]
+	# my_sentences = [[w.translate(str.maketrans('', '', string.punctuation)) for w in sent] for sent in my_sentences]
 	# remove empty strings
 	my_sentences = [[w for w in sent_words if w != ""] for sent_words in my_sentences]
+	print("[CORPUS] n_senteces: {}".format(len(my_sentences)))
 
 	# Remove stopwords
 	stop_words = set(stopwords.words('english'))   
@@ -40,9 +41,12 @@ if __name__ == '__main__':
 	# Lemmatize
 	lemmatizer = WordNetLemmatizer() 
 	my_sentences = [[lemmatizer.lemmatize(w) for w in sent] for sent in my_sentences]
+	print("[CORPUS] Finished cleaning corpus".format(len(my_sentences)))
+
 
 	# Create unique id for each token
 	word_to_id = {token: idx for idx, token in enumerate(set([w for sent in my_sentences for w in sent]))}
+
 
 	# Convert text to list of word ids
 	sentences_ids = [[word_to_id[token] for token in sent] for sent in my_sentences]
@@ -50,29 +54,29 @@ if __name__ == '__main__':
 	# Building word pairs
 	window_size = 2
 	data = []
-
-
-
 	for sent in sentences_ids:
 		for i, w in enumerate(sent):
 			for j in range(-window_size,window_size+1):
 				if j != 0:
 					if 0 <=  i+ j < len(sent):
 						data.append([w, sent[i + j]])
-
+	print("[WORD PAIRS] finished building words pairs")
 
 
 	# One hot encodings
 	n_tokens = len(word_to_id.keys())
 	one_hot_encodings = [np.eye(n_tokens)[pair] for pair in data]
+	print("[One HOT] finished encoding into one hot")
 
+	# separate words pairs into training and label
 	train = np.array(one_hot_encodings)[:, 0, :]
 	label = np.array(one_hot_encodings)[:, 1, :]
-
-
+	print("[DATA] training size: {}".format(np.shape(train)))
+	print("[DATA] training size: {}".format(np.shape(label)))
 
 
 	# model creation
+	print("[MODEL] Building model")
 	model = keras.Sequential()
 	model.add(keras.Input(shape=(n_tokens,)))
 	model.add(keras.layers.Dense(300, activation="relu"))
@@ -81,6 +85,7 @@ if __name__ == '__main__':
 	model.compile(optimizer='adam',
               loss=tf.keras.losses.CategoricalCrossentropy(from_logits=True),
               metrics=['accuracy'])
+	print("[MODEL] Model compiled!")
 
 	model.fit(train, label, epochs=100)
 
